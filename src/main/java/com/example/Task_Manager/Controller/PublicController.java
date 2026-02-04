fb9a2518b57a0e81f40df8d7994012fca9cd9933
Tejas <tejasdange40@gmail.com>
2026-02-04 08:50:19 +0530

    Added JWT authentication

--- src/main/java/com/example/Task_Manager/Controller/PublicController.java
+++ src/main/java/com/example/Task_Manager/Controller/PublicController.java
@@ -4,24 +4,44 @@ import com.example.Task_Manager.Entity.User;
 import com.example.Task_Manager.Repository.Task_Repository;
 import com.example.Task_Manager.Service.TaskService;
 import com.example.Task_Manager.Service.UserService;
+import com.example.Task_Manager.Utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/public")
 public class PublicController {
     @Autowired
     private TaskService taskService;
+
     @Autowired
     private UserService userService;
 
-    @PostMapping("/create-user")
-    public ResponseEntity<?> Create_User(@RequestBody User user){
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> Signup(@RequestBody User user){
         if(user !=null){
             userService.CreateUser(user);
             return new ResponseEntity<>(HttpStatus.CREATED);
@@ -29,4 +49,17 @@ public class PublicController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping("/Login")
+    public ResponseEntity<?> Login(@RequestBody User user){
+try {
+    authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+    UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+    String jwt = jwtUtil.generateToken(userDetails.getUsername());
+    return new ResponseEntity<>(jwt , HttpStatus.OK);
+}catch(Exception e) {
+    return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+}
+}
 }
--- src/main/java/com/example/Task_Manager/SecurityConfig/Spring_security.java
+++ src/main/java/com/example/Task_Manager/SecurityConfig/Spring_security.java
@@ -1,22 +1,32 @@
 package com.example.Task_Manager.SecurityConfig;
 
+import com.example.Task_Manager.Filter.JwtFilter;
 import com.example.Task_Manager.Service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class Spring_security {
 
-    private final UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtFilter jwtFilter;
 
+    private final UserDetailsServiceImpl userDetailsService;
+    
     public Spring_security(UserDetailsServiceImpl userDetailsService){
         this.userDetailsService = userDetailsService;
     }
@@ -28,10 +38,16 @@ public class Spring_security {
                          .requestMatchers("/public/**").permitAll()
                          .requestMatchers("/admin/**").hasRole("ADMIN")
                          .anyRequest().authenticated())
-                 .httpBasic(Customizer.withDefaults())
                  .csrf(AbstractHttpConfigurer::disable)
+                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                  .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
